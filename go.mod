860a10cfc9a6f9342b13debe47c88fe83195727e
Tyler Howle <t.howle06@outlook.com>
2026-03-30 09:54:39 +0100

    feat: add clipboard support

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module passgen-go
 
 go 1.25.6
+
+require github.com/atotto/clipboard v0.1.4
--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"passgen-go/generator"
+	"strings"
+
+	"github.com/atotto/clipboard"
 )
 
 func main() {
@@ -12,6 +15,7 @@ func main() {
 	noSymbols := flag.Bool("no-symbols", false, "Exclude symbols")
 	requireEach := flag.Bool("require-each-class", false, "Require each character class")
 	showEntropy := flag.Bool("show-entropy", false, "Show entropy")
+	clipboardFlag := flag.Bool("clipboard", false, "Copy to clipboard")
 
 	flag.Parse()
 
@@ -37,13 +41,29 @@ func main() {
 		RequireEachClass: *requireEach,
 	}
 
+	var results []string
+
 	for i := 0; i < *count; i++ {
-		fmt.Println(generator.Generate(policy))
+		pwd := generator.Generate(policy)
+		results = append(results, pwd)
+	}
+
+	for _, pwd := range results {
+		fmt.Println(pwd)
 	}
 
 	if *showEntropy {
 		entropy := generator.CalculateEntropy(policy)
 		strength := generator.ClassifyEntropy(entropy)
 		fmt.Printf("\nEntropy: %.2f bits (%s)\n", entropy, strength)
+	}
+
+	if *clipboardFlag {
+		output := strings.Join(results, "\n")
+
+		err := clipboard.WriteAll(output)
+		if err != nil {
+			fmt.Println("Clipboard error:", err)
+		}
 	}
 }
