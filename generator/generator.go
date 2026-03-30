bacfe02a11cf13aaec67f344b1cb1c1f5a817e36
Tyler Howle <t.howle06@outlook.com>
2026-03-30 09:26:52 +0100

    feat: implement secure password generation with crypto/rand and shuffle

--- generator/generator.go
+++ generator/generator.go
@@ -1,5 +1,10 @@
 package generator
 
+import (
+	"crypto/rand"
+	"math/big"
+)
+
 type Policy struct {
 	Length           int
 	IncludeUpper     bool
@@ -7,4 +12,61 @@ type Policy struct {
 	IncludeDigits    bool
 	IncludeSymbols   bool
 	RequireEachClass bool
+}
+
+var upper = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var lower = []rune("abcdefghijklmnopqrstuvwxyz")
+var digits = []rune("0123456789")
+var symbols = []rune("!@#$%^&*()-_+=?")
+
+func randomChar(set []rune) rune {
+	n, _ := rand.Int(rand.Reader, big.NewInt(int64(len(set))))
+	return set[n.Int64()]
+}
+
+func shuffle(runes []rune) {
+	for i := len(runes) - 1; i > 0; i-- {
+		jBig, _ := rand.Int(rand.Reader, big.NewInt(int64(i+1)))
+		j := int(jBig.Int64())
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+}
+
+func Generate(policy Policy) string {
+	var all []rune
+	var password []rune
+	var sets [][]rune
+
+	if policy.IncludeUpper {
+		all = append(all, upper...)
+		sets = append(sets, upper)
+	}
+	if policy.IncludeLower {
+		all = append(all, lower...)
+		sets = append(sets, lower)
+	}
+	if policy.IncludeDigits {
+		all = append(all, digits...)
+		sets = append(sets, digits)
+	}
+	if policy.IncludeSymbols {
+		all = append(all, symbols...)
+		sets = append(sets, symbols)
+	}
+
+	// Require each class
+	if policy.RequireEachClass {
+		for _, set := range sets {
+			password = append(password, randomChar(set))
+		}
+	}
+
+	// Fill remaining
+	for len(password) < policy.Length {
+		password = append(password, randomChar(all))
+	}
+
+	shuffle(password)
+
+	return string(password)
 }
