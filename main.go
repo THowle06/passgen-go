8f481a69c8ab501163caed70ef53afa068f8db50
Tyler Howle <t.howle06@outlook.com>
2026-03-30 09:17:11 +0100

    feat: add CLI flags using standard library

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Password generator placeholder")
+	length := flag.Int("length", 16, "Length of password")
+	count := flag.Int("count", 1, "Number of passwords")
+	noSymbols := flag.Bool("no-symbols", false, "Exclude symbols")
+	requireEach := flag.Bool("require-each-class", false, "Require each character class")
+	showEntropy := flag.Bool("show-entropy", false, "Show entropy")
+
+	flag.Parse()
+
+	fmt.Println(*length, *count, *noSymbols, *requireEach, *showEntropy)
 }
